c2ebce794b6456520dab3d52e8bd412e2641753c
Yosnier Q <taghil>
2020-04-21 21:44:29 -0500

    Api de Perros

---
+++ index.js
@@ -0,0 +1,61 @@
+
+		function getData(url){
+
+  return new Promise((resolve, reject) => {
+    var req = new XMLHttpRequest();
+  	req.open('GET', url);
+    
+    req.onload = function() {
+      if (req.status == 200) {
+        resolve(req.response);
+      }
+      else {
+        reject(Error(req.statusText));
+      }
+    }
+ 
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    }
+
+    req.send()
+   
+  })
+}
+
+// var data;
+
+function dataImg(date) {
+	document.getElementById('img').src = date
+}
+
+
+
+function aloha() {
+
+var userValue = document.getElementById('userCan').value;
+var link = "/" + userValue;
+if (userValue != false) 
+{
+
+  	var division = document.getElementById('containere');
+	// division.style = "font-size: 30px";
+	division.innerHTML = "Felicidades! Su perro favorito es " + userValue;
+
+getData(`https://dog.ceo/api/breed${link}/images/random`)
+  .then((respuesta) => {
+    data = JSON.parse(respuesta)
+  })
+  .then(() => {
+    dataImg(data.message);
+  })
+
+} else {
+	var division = document.getElementById('containere');
+	division.style = "font-size: 50px";
+	division.innerHTML = "Disculpe, ingrese el nombre de su perro favorito por favor"
+}
+
+
+
+}
